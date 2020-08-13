5398b36c0d45867bbe0e7a60c16f69a26f7c0026
Anson <git@anson-cheung.com>
2020-08-13 16:59:01 +0800

    Add demo display

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
-#define LED_PORT 13
+#include <U8g2lib.h>
+#include <SPI.h>
+#include <Wire.h>
+
+U8G2_SH1107_SEEED_128X128_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
 void setup() {
-  pinMode(LED_PORT, OUTPUT);
+  u8g2.begin();
 }
 
 void loop() {
-  digitalWrite(LED_PORT, HIGH);
-  delay(1000);
-  digitalWrite(LED_PORT, LOW);
-  delay(1000);
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_ncenB10_tr);
+    u8g2.drawStr(0,24,"Hello, world!");
+  } while ( u8g2.nextPage() );
 }
