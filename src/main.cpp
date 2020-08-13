811d5c55da08bb3fa2ed48cbf4b0ce506d00d819
Anson <git@anson-cheung.com>
2020-08-13 18:14:28 +0800

    Add a button that changes text

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,13 @@
 
 U8G2_SH1107_SEEED_128X128_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
+const int BUTTON_PIN = 21;
+const int LED_PIN = 13;
+
 void setup() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  
   u8g2.begin();
 }
 
@@ -13,6 +19,16 @@ void loop() {
   u8g2.firstPage();
   do {
     u8g2.setFont(u8g2_font_ncenB10_tr);
-    u8g2.drawStr(0,24,"Hello, world!");
+
+    int button = digitalRead(BUTTON_PIN);
+
+    if (button == LOW) { // Not sure why this is LOW
+      u8g2.drawStr(0,24,"Button pressed");
+      digitalWrite(LED_PIN, HIGH);
+    }
+    else {
+      u8g2.drawStr(0,24,"Hello, world!");
+    }
+
   } while ( u8g2.nextPage() );
 }
