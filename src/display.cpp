5ef0e069237e98c6e80132fbcc7a9c92da0605e4
Anson <git@anson-cheung.com>
2020-08-14 22:21:30 +0800

    Add divider

--- src/display.cpp
+++ src/display.cpp
@@ -7,30 +7,32 @@
 U8G2_SH1107_SEEED_128X128_F_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
 void drawMainScreen() {
+    u8g2.drawVLine(62, 0, 128);
     drawNDL();
     drawNextStopDepth();
+    u8g2.drawHLine(0, 41, 128);
     drawDepth();
     drawRuntime();
+    u8g2.drawHLine(0, 87, 128);
     drawGas();
     drawTTS();
-
     u8g2.sendBuffer();
 }
 
 void drawNDL() {
     u8g2.setFont(u8g2_font_9x15B_mr);
-    u8g2.drawStr(0, 12, "NDL");
+    u8g2.drawStr(0, 12, "Time");
 
     u8g2.setFont(u8g2_font_inb21_mn);
-    u8g2.drawStr(0, 36, "999");
+    u8g2.drawStr(0, 36, "4");
 }
 
 void drawNextStopDepth() {
     u8g2.setFont(u8g2_font_9x15B_mr);
-    u8g2.drawStr(75, 12, "Stop");
+    u8g2.drawStr(70, 12, "Stop");
 
     u8g2.setFont(u8g2_font_inb21_mn);
-    u8g2.drawStr(75, 36, "99");
+    u8g2.drawStr(70, 36, "6");
 }
 
 void drawDepth() {
@@ -38,15 +40,15 @@ void drawDepth() {
     u8g2.drawStr(0, 58, "Depth");
 
     u8g2.setFont(u8g2_font_inb21_mn);
-    u8g2.drawStr(0, 82, "99");
+    u8g2.drawStr(0, 82, "25");
 }
 
 void drawRuntime() {
     u8g2.setFont(u8g2_font_9x15B_mr);
-    u8g2.drawStr(65, 58, "Run");
+    u8g2.drawStr(70, 58, "Run");
 
     u8g2.setFont(u8g2_font_inb21_mn);
-    u8g2.drawStr(65, 82, "999");
+    u8g2.drawStr(70, 82, "42");
 }
 
 void drawGas() {
@@ -54,13 +56,13 @@ void drawGas() {
     u8g2.drawStr(0, 104, "Gas");
 
     u8g2.setFont(u8g2_font_10x20_mn);
-    u8g2.drawStr(0, 128, "99/99");
+    u8g2.drawStr(0, 128, "21/0");
 }
 
 void drawTTS() {
     u8g2.setFont(u8g2_font_9x15B_mr);
-    u8g2.drawStr(65, 104, "TTS");
+    u8g2.drawStr(70, 104, "TTS");
 
     u8g2.setFont(u8g2_font_inb21_mn);
-    u8g2.drawStr(65, 128, "999");
+    u8g2.drawStr(70, 128, "15");
 }
