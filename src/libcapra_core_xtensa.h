9400a44f5fe34ebf9bbc23984647e4f7a6e623da
Anson <git@anson-cheung.com>
2020-10-06 18:42:14 +0800

    Add demo zhl16

--- src/libcapra_core_xtensa.h
+++ src/libcapra_core_xtensa.h
@@ -50,6 +50,42 @@ struct CDiveSegment {
   intptr_t descent_rate;
 };
 
+/// A set of tissues for use in decompression models, comprising a set of tissues for nitrogen
+/// and another set for helium.
+struct Tissue {
+  double p_n2[TISSUE_COUNT];
+  double p_he[TISSUE_COUNT];
+  double p_t[TISSUE_COUNT];
+};
+
+/// A ZHL-16 decompression model of a diver.
+/// # Notes
+/// For now, each ZHL16 struct should only be used for one dive. This is because calculating decompression
+/// stops with Gradient Factors requires some side effects to be stored inside the struct.
+struct ZHL16 {
+  /// Current tissue model of the diver.
+  Tissue tissue;
+  /// Current depth of the diver.
+  uintptr_t diver_depth;
+  /// Nitrogen A-values.
+  double n2_a[TISSUE_COUNT];
+  /// Nitrogen B-values.
+  double n2_b[TISSUE_COUNT];
+  /// Nitrogen half-lives.
+  double n2_hl[TISSUE_COUNT];
+  /// Helium A-values.
+  double he_a[TISSUE_COUNT];
+  /// Helium B-values.
+  double he_b[TISSUE_COUNT];
+  /// Helium half-lives.
+  double he_hl[TISSUE_COUNT];
+  uintptr_t first_deco_depth;
+  /// GF Low value
+  double gf_low;
+  /// GF High value
+  double gf_high;
+};
+
 /// A gas mix used in a dive.
 struct Gas {
   /// Percentage fraction of oxygen in the mix.
@@ -62,14 +98,13 @@ struct Gas {
 
 extern "C" {
 
-CDiveSegment get_next_stop(const Gas *gas, intptr_t ascent_rate, intptr_t descent_rate);
+CDiveSegment get_next_stop(const ZHL16 *deco,
+                           const Gas *gas,
+                           intptr_t ascent_rate,
+                           intptr_t descent_rate);
 
-void initialise();
+void initialise(ZHL16 *deco);
 
-void set_gfh(uintptr_t high);
-
-void set_gfl(uintptr_t low);
-
-void tick_segment(const Gas *gas, uintptr_t depth, uint64_t tick);
+void tick_segment(ZHL16 *deco, const Gas *gas, uintptr_t depth, uint64_t tick);
 
 } // extern "C"
--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,19 @@
 #include "display.hpp"
 #include "button.hpp"
 #include "menu.hpp"
-// #include "libcapra_core_xtensa.h"
-#include "ltx.h"
+#include "libcapra_core_xtensa.h"
+// #include "ltx.h"
 
 const int LED_PIN = 13;
+
+ZHL16 deco;
 
 void setup() {
   pinMode(BUTTON_CYCLE_PIN, INPUT_PULLUP);
   pinMode(BUTTON_SELECT_PIN, INPUT_PULLUP);
 
   pinMode(LED_PIN, OUTPUT);
+  initialise(&deco);
   
   
   u8g2.begin(BUTTON_SELECT_PIN, BUTTON_CYCLE_PIN, U8X8_PIN_NONE, U8X8_PIN_NONE, U8X8_PIN_NONE, U8X8_PIN_NONE);
@@ -23,9 +26,10 @@ void setup() {
 
 void loop() {
 
-  if (initialise() == 42069) {
-    digitalWrite(LED_PIN, HIGH);
-  }
+  // if (initialise() == 42069) {
+  //   digitalWrite(LED_PIN, HIGH);
+  // }
+
 
   if (cycleButtonPressed()) {
     current_state = nextDisplayState(current_state, Cycle);
@@ -35,7 +39,13 @@ void loop() {
   }
 
   u8g2.clearBuffer();
-  drawScreen(current_state);
+  // drawScreen(current_state);
+
+  char test[16];
+  sprintf(test, "%f", deco.n2_hl[15]);
+  u8g2.setFont(u8g2_font_inr27_mf);
+  u8g2.drawStr(0, 100, test);
+  u8g2.sendBuffer();
 
   updateCycleButtonState();
   updateSelectButtonState();
